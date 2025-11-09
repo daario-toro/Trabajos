2fdc8ba08117dc092cc50dae2a97efa9897cfabe
catalina <catalinaortuno98@gmail.com>
2025-11-09 13:00:14 -0300

    modifico rama dario

--- prueba-ramas.py
+++ prueba-ramas.py
@@ -2,3 +2,4 @@ print("probando ramas")
 print("probando2")
 #probando ramas
 print("Rama nueva creada desde GitHub Dario")
+#texto agregado desde Visual Studio Codeprint("Otro cambio desde VS Code en la rama nueva")
