c3c525b1725ac2401f3970c60da8a39d387a13f1
catalina <catalinaortuno98@gmail.com>
2025-11-09 13:12:42 -0300

    corregi errores no habia guardado

--- prueba-ramas.py
+++ prueba-ramas.py
@@ -1,10 +1,10 @@
 print("probando ramas")
 print("probando2")
-<<<<<<< HEAD
+#<<<<<<< HEAD
 print("rama catalina")
 print("Rama nueva creada desde GitHub Dario")
-=======
+#=======
 #probando ramas
 print("Rama nueva creada desde GitHub Dario")
 #texto agregado desde Visual Studio Codeprint("Otro cambio desde VS Code en la rama nueva")
->>>>>>> dario
+#>>>>>>> dario
